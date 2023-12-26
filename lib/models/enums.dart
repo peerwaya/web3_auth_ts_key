7e2bd45b66d8487c9b50056a755d0e3bbcd66e3e
Onyemaechi Okafor <onyemaechiokafor@Onyemaechis-MacBook-Pro.local>
2023-12-26 04:52:13 +0100

    remove unused

--- lib/models/enums.dart
+++ lib/models/enums.dart
@@ -1,4 +1,4 @@
-import 'package:customauth_flutter/customauth_flutter.dart';
+//import 'package:customauth_flutter/customauth_flutter.dart';
 
 enum Network { mainnet, testnet, cyan, aqua, sapphire_devnet, sapphire_mainnet }
 
@@ -6,13 +6,13 @@ enum ChainNamespace { eip155, solana }
 
 enum BuildEnv { production, staging, testing }
 
-extension TorusNetworkExt on Network {
-  TorusNetwork toTorusNetworl() => switch (this) {
-        Network.mainnet => TorusNetwork.mainnet,
-        Network.testnet => TorusNetwork.testnet,
-        Network.aqua => TorusNetwork.aqua,
-        Network.cyan => TorusNetwork.cyan,
-        Network.sapphire_devnet => TorusNetwork.testnet,
-        Network.sapphire_mainnet => TorusNetwork.mainnet,
-      };
-}
+// extension TorusNetworkExt on Network {
+//   TorusNetwork toTorusNetworl() => switch (this) {
+//         Network.mainnet => TorusNetwork.mainnet,
+//         Network.testnet => TorusNetwork.testnet,
+//         Network.aqua => TorusNetwork.aqua,
+//         Network.cyan => TorusNetwork.cyan,
+//         Network.sapphire_devnet => TorusNetwork.testnet,
+//         Network.sapphire_mainnet => TorusNetwork.mainnet,
+//       };
+// }
